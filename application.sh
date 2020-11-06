8817b6cec857fac8ad210937e470425ac7dd7385
DanHutsul <dan.hutsul@gmail.com>
2020-11-06 20:50:29 +0200

    Initial commit

---
+++ application.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# OPTIONAL
+
+# Shell script that can be useful for automation some processes.
+# For example: 
+###
+#
+#FILE_IN=${argc}[1]
+#FILE_OUT=${argc}[2]
+#
+#mkdir -p build
+#mkdir -p bin
+#
+#cd build
+#cmake -G"Unix Makefiles" ..
+#make
+#cd ../bin/
+#chmod u+x main
+
+
+set -o errexit
+set -o nounset
+set -o pipefail
